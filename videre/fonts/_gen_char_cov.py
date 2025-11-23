6e8d25ce16d3e85acb12e2b66960abbd8b7b6da7
notoraptor <notoraptor@users.noreply.github.com>
2025-11-22 22:31:47 -0500

    Add simple test for fonts and char coverage generation

--- videre/fonts/_gen_char_cov.py
+++ videre/fonts/_gen_char_cov.py
@@ -3,6 +3,7 @@ Main script to generate char-support.json
 """
 
 import json
+import logging
 import os
 from typing import Self, Sequence
 
@@ -10,13 +11,10 @@ from fontTools.ttLib import TTCollection
 from tqdm import tqdm
 
 from videre.fonts.font_utils import FontUtils
-from videre.fonts.provider import (
-    FOLDER_FONT,
-    FONT_NOTO_REGULAR,
-    FontProvider,
-    get_fonts,
-)
+from videre.fonts.provider import FOLDER_FONT, FONT_NOTO_REGULAR, get_fonts
 from videre.fonts.unicode_utils import Unicode
+
+logger = logging.getLogger(__name__)
 
 
 class CharFontPriority:
@@ -48,6 +46,51 @@ class CharFontPriority:
             return self.rank < other.rank
 
 
+def generate_char_cov():
+    char_to_font = generate_char_to_font()
+    _gen_font_to_characters(char_to_font)
+
+
+def generate_char_to_font(
+    priority_fonts: Sequence[str] = (FONT_NOTO_REGULAR.name,),
+) -> dict[str, str]:
+    font_to_rank = {name: order for order, name in enumerate(priority_fonts)}
+    font_to_block_to_cov: dict[str, dict[str, list[str]]] = {}
+
+    fonts = _load_fonts()
+
+    char_to_fonts = {}
+    for font in fonts:
+        coverage = font.coverage()
+        font_to_block_to_cov[font.name] = coverage
+        for block, covered_chars in coverage.items():
+            for c in covered_chars:
+                char_to_fonts.setdefault(c, []).append(font.name)
+    nb_unicode = len(list(Unicode.characters()))
+    nb_covered = len(char_to_fonts)
+
+    char_to_font: dict[str, str] = {
+        c: (
+            names[0]
+            if len(names) == 1
+            else sorted(
+                names,
+                key=lambda name: CharFontPriority(
+                    name, c, font_to_block_to_cov[name], font_to_rank
+                ),
+            )[0]
+        )
+        for c, names in tqdm(char_to_fonts.items(), desc="Mapping char => font")
+    }
+
+    logger.info(
+        f"fonts: {len(set(char_to_font.values()))} / {len(fonts)}, "
+        f"characters: {nb_covered} / {nb_unicode} "
+        f"({round(nb_covered * 100 / nb_unicode, 2)} %)"
+    )
+    return char_to_font
+
+
 def _load_fonts(font_table: dict[str, str] | None = None) -> list[FontUtils]:
     font_table = font_table or get_fonts()
     fonts = []
@@ -62,56 +105,6 @@ def _load_fonts(font_table: dict[str, str] | None = None) -> list[FontUtils]:
     return fonts
 
 
-def generate_char_cov(priority_fonts: Sequence[str] = (FONT_NOTO_REGULAR.name,)):
-    font_to_rank = {name: order for order, name in enumerate(priority_fonts)}
-    font_to_block_to_cov: dict[str, dict[str, list[str]]] = {}
-
-    fonts = _load_fonts()
-    print("Fonts:", len(fonts))
-
-    char_to_fonts = {}
-    for font in fonts:
-        coverage = font.coverage()
-        font_to_block_to_cov[font.name] = coverage
-        for block, covered_chars in coverage.items():
-            for c in covered_chars:
-                char_to_fonts.setdefault(c, []).append(font.name)
-    nb_unicode = len(list(Unicode.characters()))
-    nb_covered = len(char_to_fonts)
-    print("Characters:", nb_unicode)
-    print("Covered:", nb_covered, f"({round(nb_covered * 100 / nb_unicode, 2)} %)")
-
-    char_to_font: dict[str, str] = {}
-    for c, names in tqdm(char_to_fonts.items()):
-        if len(names) == 1:
-            (selected_name,) = names
-        else:
-            selected_name = sorted(
-                names,
-                key=lambda name: CharFontPriority(
-                    name, c, font_to_block_to_cov[name], font_to_rank
-                ),
-            )[0]
-        char_to_font[c] = selected_name
-
-    char_support = _gen_char_support(char_to_font, save=False)
-    font_to_characters = _gen_font_to_characters(char_to_font)
-    _compare_output_formats(char_support, font_to_characters)
-
-
-def _gen_char_support(char_to_font: dict[str, str], save=True):
-    selected_fonts = sorted(set(char_to_font.values()))
-    print(f"Selected fonts: {len(selected_fonts)}")
-    selected_indices = {name: i for i, name in enumerate(selected_fonts)}
-    assert len(selected_fonts) == len(selected_indices)
-    char_to_indice = {c: selected_indices[name] for c, name in char_to_font.items()}
-    output = {"fonts": selected_fonts, "characters": char_to_indice}
-    if save:
-        with open(os.path.join(FOLDER_FONT, "char-support.json"), "w") as file:
-            json.dump(output, file)
-    return output
-
-
 def _gen_font_to_characters(char_to_font: dict[str, str], save=True) -> dict[str, str]:
     font_to_chars = {}
     for char, font_name in char_to_font.items():
@@ -119,25 +112,11 @@ def _gen_font_to_characters(char_to_font: dict[str, str], save=True) -> dict[str
     font_to_characters = {
         font_name: "".join(chars) for font_name, chars in font_to_chars.items()
     }
-    print("Used fonts:", len(font_to_characters))
     if save:
         with open(os.path.join(FOLDER_FONT, "font-to-characters.json"), "w") as file:
             json.dump(font_to_characters, file)
     return font_to_characters
 
 
-def _compare_output_formats(char_support: dict, font_to_characters: dict[str, str]):
-    fonts, char_to_indice = FontProvider._parse_font_to_characters(font_to_characters)
-    assert fonts
-    assert char_to_indice
-    assert fonts == char_support["fonts"]
-    assert char_to_indice == char_support["characters"]
-
-
-def main():
-    # check_characters_coverage("☐☑✅✓✔🗸🗹")
-    generate_char_cov()
-
-
 if __name__ == "__main__":
-    main()
+    generate_char_cov()
---
+++ videre_tests/test_fonts.py
@@ -0,0 +1,32 @@
+from typing import TypedDict
+
+from videre.fonts._gen_char_cov import _gen_font_to_characters, generate_char_to_font
+from videre.fonts.provider import FontProvider
+
+
+class CharSupport(TypedDict):
+    fonts: list[str]
+    characters: dict[str, int]
+
+
+def _gen_char_support(char_to_font: dict[str, str]) -> CharSupport:
+    selected_fonts = sorted(set(char_to_font.values()))
+    selected_indices = {name: i for i, name in enumerate(selected_fonts)}
+    assert len(selected_fonts) == len(selected_indices)
+    char_to_indice = {c: selected_indices[name] for c, name in char_to_font.items()}
+    return {"fonts": selected_fonts, "characters": char_to_indice}
+
+
+def test_generate_char_cov():
+    char_to_font = generate_char_to_font()
+    char_support = _gen_char_support(char_to_font)
+    font_to_characters = _gen_font_to_characters(char_to_font, save=False)
+
+    assert len(char_to_font) == 111137
+    assert len(set(char_to_font.values())) == 165
+
+    fonts, char_to_indice = FontProvider._parse_font_to_characters(font_to_characters)
+    assert fonts
+    assert char_to_indice
+    assert fonts == char_support["fonts"]
+    assert char_to_indice == char_support["characters"]
